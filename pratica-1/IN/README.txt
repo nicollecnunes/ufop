c5913d993265dafed9996e13d818257ea9734998
fallore <nicollecanuton@gmail.com>
2021-05-27 13:49:07 -0300

    mudando o diretorio

---
+++ pratica-1/IN/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 27/05/2021 13:44:20 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: Aula Prática 1 - Manipulação de Matrizes Dinâmicas
---
+++ pratica-1/prototipo1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    double **M;
+    /*
+    - leia o valor de n
+    - aloque a matriz dinamicamente
+    - leia a operacao
+    - leia o conteudo da matriz
+    - calcule a soma
+    - se necessario, calcule a media
+    - imprima o resultado no formato correto
+    - libere a memoria
+    */
+    return 0;//nao remova
+}
