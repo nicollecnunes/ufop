8728c49a0bc01ae5ed86d3447a5e5b5a45aca33f
fallore <nicollecanuton@gmail.com>
2021-05-27 13:55:55 -0300

    alocação matriz

--- pratica-1/prototipo1.c
+++ pratica-1/prototipo1.c
@@ -2,15 +2,45 @@
 int main()
 {
     double **M;
-    /*
-    - leia o valor de n
-    - aloque a matriz dinamicamente
-    - leia a operacao
-    - leia o conteudo da matriz
-    - calcule a soma
-    - se necessario, calcule a media
-    - imprima o resultado no formato correto
-    - libere a memoria
-    */
-    return 0;//nao remova
+
+    char O[0];
+    int aux = 0, i, j, n;
+    double soma = 0;
+
+    scanf("%d", &n);
+    M = (int**) malloc(sizeof(int*)* *n);
+    for(i=0;i<*n;i++){
+		M[i] = (int*) malloc(sizeof(int)* *n); 
+	}
+
+    scanf("%c", &O[0]);
+
+
+    for (i=0; i<12; i++){
+    	for (j=0; j<12; j++){
+    		scanf("%lf",&M[i][j]);
+    	}   	
+	}
+
+	for (i=0; i<5; i++){
+		for(j=i+1; j<11-aux; j++){
+			soma = M[i][j] + soma;
+		}
+		aux = aux + 1;
+	}
+
+
+	if(O[0]=='M'){
+		soma = soma/30;
+	}
+
+	printf("%.1lf\n", soma);
+
+	for(i=0;i<n;i++){
+		free(M[i]);
+	}
+	free(M);
+
+    return 0;
 }
+
