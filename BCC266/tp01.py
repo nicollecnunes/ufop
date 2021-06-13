3ddbd89adfcaf0b21a2379965386552a914835fd
fallore <nicollecanuton@gmail.com>
2021-06-13 18:52:09 -0300

    programa de divisões

--- BCC266/tp01.py
+++ BCC266/tp01.py
@@ -12,46 +12,45 @@ def iniciaRAM():
         RAM[i] = random.randint(0, 1000)
         #print("meu deus parece q essa ram nao tem é nada uai\n")
         
+def maquinaInt(umaInstrucao):
+    opcode = umaInstrucao[0];
+    if opcode == 0: #soma
+        end1 = int(umaInstrucao[1])
+        end2 = int(umaInstrucao[2])
+        #print("end1 = {} e end2={}".format(end1, end2))
+        soma = RAM[end1] + RAM[end2]
+        end3 = int(umaInstrucao[3])
+        print("Somando {} com {} e obtendo {}\n".format(RAM[end1], RAM[end2], soma))
+        RAM[end3] = soma        
+    elif opcode == 1: #subtrai
+        end1 = int(umaInstrucao[1])
+        end2 = int(umaInstrucao[2])
+        #print("end1 = {} e end2={}".format(end1, end2))
+        sub = RAM[end1] - RAM[end2]
+        end3 = int(umaInstrucao[3])
+        RAM[end3] = sub       
+    elif opcode == 2: #levar pra memoria
+        conteudo = int(umaInstrucao[1])
+        end = int(umaInstrucao[2])
+        RAM[end] = conteudo
+    elif opcode == 3: #trazer
+        end = int(umaInstrucao[2])
+        retorno = RAM[end]
+        
 def maquina():
     PC = 0 #primeira instrução a ser processada
     opcode = INT_MAX
     while opcode != -1:
         umaInstrucao = [0,0,0,0]
         umaInstrucao = memoriaInstrucoes[PC]
-        opcode = umaInstrucao[0] 
-        #a "maquina interpretada" ta aqui mesmo
-        if opcode == 0: #soma
-            end1 = int(umaInstrucao[1])
-            end2 = int(umaInstrucao[2])
-            #print("end1 = {} e end2={}".format(end1, end2))
-            cntRAM1 = RAM[end1]
-            cntRAM2 = RAM[end2]
-            soma = cntRAM1 + cntRAM2
-            end3 = int(umaInstrucao[3])
-            ("Somando {} com {} e obtendo {}\n".format(cntRAM1, cntRAM2, soma))
-            RAM[end3] = soma        
-        elif opcode == 1: #subtrai
-            end1 = int(umaInstrucao[1])
-            end2 = int(umaInstrucao[2])
-            #print("end1 = {} e end2={}".format(end1, end2))
-            cntRAM1 = RAM[end1]
-            cntRAM2 = RAM[end2]
-            sub = cntRAM1 - cntRAM2
-            end3 = int(umaInstrucao[3])
-            print("Subtraindo {} de {} e obtendo {}\n".format(cntRAM1, cntRAM2, sub))
-            RAM[end3] = sub       
-        elif opcode == 2: #levar pra memoria
-            conteudo = int(umaInstrucao[1])
-            end = int(umaInstrucao[2])
-            RAM[end] = conteudo
-        elif opcode == 3: #trazer
-            end = int(umaInstrucao[2])
-            retorno = RAM[end]
+        opcode = umaInstrucao[0]
+        
+        maquinaInt(umaInstrucao)
         PC = PC +1
 
         
 def montarInstAleatorias():
-    umaInstrucao = [0,0,0,0]
+    umaInstrucao = [0] * 4
     for i in range(99):#mudar pra 99
         umaInstrucao[0] = random.randint(0,1) #gera 0 ou 1
         umaInstrucao[1] = random.randint(0, 99)
@@ -60,42 +59,32 @@ def montarInstAleatorias():
         memoriaInstrucoes[i] = umaInstrucao
         #print(memoriaInstrucoes[i])
         
-    umaInstrucao[0] = -1
-    umaInstrucao[1] = -1
-    umaInstrucao[2] = -1
-    umaInstrucao[3] = -1
+    umaInstrucao = [-1] * 4 #halt
     memoriaInstrucoes[99] = umaInstrucao
     #print(memoriaInstrucoes[99])
     
     maquina()
         
 def instSoma(end1, end2, end3):
-    umaInstrucao = [0] * 4
-    umaInstrucao[0] = 0 #opcode de soma
-    umaInstrucao[1] = end1
-    umaInstrucao[2] = end2
-    umaInstrucao[3] = end3
+    umaInstrucao = [0,end1,end2,end3]
     return umaInstrucao
 
 def instSubt(end1, end2, end3):
-    umaInstrucao = [0,0,0,0]
-    umaInstrucao[0] = 1 #opcode de subtração
-    umaInstrucao[1] = end1
-    umaInstrucao[2] = end2
-    umaInstrucao[3] = end3
+    umaInstrucao = [1,end1,end2,end3]
     return umaInstrucao
 
 
 def instLevaPMemo(x, end): 
-    umaInstrucao = [0,0,0,0]
-    umaInstrucao[0] = 2
-    umaInstrucao[1] = x
-    umaInstrucao[2] = end
-    umaInstrucao[3] = -1
+    umaInstrucao = [2,x,end,-1]
     return umaInstrucao
 
-def TrazMemo(end):
-    return(RAM[end])
+def trazMemo(end):
+    umaInstrucao = [3,-1,end,-1]
+    maquinaInt(umaInstrucao)
+    
+    end = umaInstrucao[2]
+    res = RAM[end]
+    return res
     
 
 
@@ -116,15 +105,18 @@ def somar(num1, num2):
     maquina() #maquina leva pra memoria. sem parametro pq e variavel global
 
     umaInstrucao = instSoma(0, 1, 1) #end1 + end1 e guarda o resultado no end1
-    memoriaInstrucoes[0] = umaInstrucao
+    memoriaInstrucoes[0] = umaInstrucao #montou 0/0/1/1
     
     umaInstrucao = [-1] * 4 #halt
     memoriaInstrucoes[1] = umaInstrucao #mem. inst ficou assim:
-    #0/end1/end2/end1
+    #0/end 0/1/end 1
     #-1/-1/-1/1
     maquina()
     
-    return RAM[1]
+    resultado = trazMemo(1)
+    maquinaInt(umaInstrucao)
+    
+    return resultado
     
     
 def subtrair(num1, num2):
@@ -147,10 +139,13 @@ def subtrair(num1, num2):
 
     maquina()
     
-    return RAM[1]
+    resultado = trazMemo(1)
+    maquinaInt(umaInstrucao)
+    
+    return resultado
 
 def multip(num1, num2): #??
-    umaInstrucao = [0,0,0,0]
+    umaInstrucao = [0] * 4
     
     umaInstrucao = instLevaPMemo(num1, 0)#leva o valor n1 pra posicao 0
     memoriaInstrucoes[0] = umaInstrucao
@@ -168,42 +163,47 @@ def multip(num1, num2): #??
 
     maquina() #maquina leva pra memoria. sem parametro pq e variavel global
     
-    return RAM[1]
+    resultado = trazMemo(1)
+    maquinaInt(umaInstrucao)
+    
+    return resultado
 
 def div(num1, num2):
-    aux = num1
-    umaInstrucao = [0,0,0,0]
+    #0 soma; 1 sub; 3 traz; 2 leva
+    umaInstrucao = [0] * 4
     
-    umaInstrucao = instLevaPMemo(num2, 0)#leva o valor n2 pra posicao 0
+    umaInstrucao = instLevaPMemo(num2, 0)#leva o valor n2 pra posicao 0 (2/num2/0/-1)
     memoriaInstrucoes[0] = umaInstrucao
     
-    umaInstrucao = instLevaPMemo(num1, 1)#leva o valor n1 pra posicao 1
+    umaInstrucao = instLevaPMemo(num1, 1)#leva o valor n1 pra posicao 1 (2/num1/1-1)
     memoriaInstrucoes[1] = umaInstrucao
     
-    umaInstrucao = instLevaPMemo(1, 2)#leva o valor 1 pra posicao 2. vai ser o aux
-    memoriaInstrucoes[2] = umaInstrucao
+    umaInstrucao = instLevaPMemo(1, 2)#leva o valor 1 pra posicao 2. vou usar pra somar no res
+    memoriaInstrucoes[2] = umaInstrucao #(2/1/2/-1)
+    
+    umaInstrucao = instLevaPMemo(0,3) #leva 0 pro end 3. vai ser o resultado
+    memoriaInstrucoes[3] = umaInstrucao #2/0/3/-1
     
     umaInstrucao = [-1] * 4 #halt
-    memoriaInstrucoes[3] = umaInstrucao
+    memoriaInstrucoes[4] = umaInstrucao
     
     maquina()
     
-    ram0 = 
+    ram0 = trazMemo(0) #traz o n2 do end0
+    ram1 = trazMemo(0) #traz o n2 do end0
     
-    for i in range(num1): #tentar criar uma funcao pra ficar aqui dentro
-        umaInstrucao = instSubt(1, 0, 1) #vai montar 1 | 1 | 0 | 1
-        # subt | end0 tem o num2 | end1 tem num1 | é pra onde vai o resultado (substituir). nas outras vezes do for, ja vai reaproveitar    
-        memoriaInstrucoes[i+3] = umaInstrucao #+2 pq ja usei 0 e 1
-        aux = subtrair(aux, num2)
-        if (aux<=num2):
-            break
-    
-    umaInstrucao = [-1] * 4 #halt
-    memoriaInstrucoes[num2+2] = umaInstrucao #multiplicador pq i vai ate ele +2
-
-    maquina() #maquina leva pra memoria. sem parametro pq e variavel global
-    
-    return 0
+    while(ram0<=ram1):
+        umaInstrucao = instSubt(1,0,1) #subtrai num1(end1) de num2(end0) e coloca no end 1
+        maquinaInt(umaInstrucao)
+        umaInstrucao = instSoma(2,3,3) #soma o end2+end3 e poe no end3 (resultado)
+        maquinaInt(umaInstrucao)
+        
+        ram1 = trazMemo(1) #traz a subtraçao parcial do end1
+        
+    resultado = trazMemo(3)
+    maquinaInt(umaInstrucao)
+        
+    return resultado
     
 
 def menu():
@@ -234,22 +234,21 @@ while op != -1:
     elif op == 2:
         num1 = int(input("Digite o primeiro valor: "))
         num2 = int(input("Digite o segundo valor: "))
-        somar(num1, num2)
         print("Soma de {:.0f} + {:.0f} = {}".format(num1, num2,somar(num1,num2)))
     elif op == 3:
         num1 = int(input("Digite o minuendo: "))
         num2 = int(input("Digite o subtraendo: "))
-        subtrair(num1, num2)
         print("Subtração de {:.0f} - {:.0f} = {}".format(num1, num2,subtrair(num1, num2)))
     elif op==4:
-        num1 = int(input("Digite o multiplicando (de preferência  o menor número): "))
+        num1 = int(input("Digite o multiplicando: "))
         num2 = int(input("Digite o multiplicador: "))
-        multip(num1, num2)
-        print("Multiplicação de {:.0f} x {:.0f} = {}".format(num1, num2,multip(num1, num2)))
+        if(num1>=num2):
+            print("Multiplicação de {:.0f} x {:.0f} = {}".format(num1, num2,multip(num1, num2)))
+        else:
+            print("Multiplicação de {:.0f} x {:.0f} = {}".format(num1, num2,multip(num2, num1)))
     elif op==5:
         num1 = int(input("Digite o dividendo: "))
         num2 = int(input("Digite o divisor: "))
-        div(num1, num2)
         print("Divisão de {:.0f} : {:.0f} = {}".format(num1, num2,div(num1, num2)))
         
     else:
