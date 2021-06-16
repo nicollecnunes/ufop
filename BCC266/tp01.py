b44ebcee8d8ad0ccfae5400592809916807b8cf9
fallore <nicollecanuton@gmail.com>
2021-06-15 22:58:31 -0300

    programas de geometria espacial

--- BCC266/tp01.py
+++ BCC266/tp01.py
@@ -32,6 +32,7 @@ def maquinaInt(umaInstrucao):
     elif opcode == 2: #levar pra memoria
         conteudo = int(umaInstrucao[1])
         end = int(umaInstrucao[2])
+        #print("levando pra memoria {} o {}".format(end, conteudo) )
         RAM[end] = conteudo
     elif opcode == 3: #trazer
         end = int(umaInstrucao[2])
@@ -39,11 +40,12 @@ def maquinaInt(umaInstrucao):
         
 def maquina():
     PC = 0 #primeira instrução a ser processada
-    opcode = INT_MAX
+    opcode = 0
     while opcode != -1:
         umaInstrucao = [0,0,0,0]
         umaInstrucao = memoriaInstrucoes[PC]
         opcode = umaInstrucao[0]
+        #print("estou aqui\n")
         
         maquinaInt(umaInstrucao)
         PC = PC +1
@@ -64,15 +66,8 @@ def montarInstAleatorias():
     #print(memoriaInstrucoes[99])
     
     maquina()
-        
-def instSoma(end1, end2, end3):
-    umaInstrucao = [0,end1,end2,end3]
-    # 0 | end1 | end2 |endd3
-    return umaInstrucao
 
-def instSubt(end1, end2, end3):
-    umaInstrucao = [1,end1,end2,end3]
-    return umaInstrucao
+# =============================================== OPERAÇÕES BÁSICAS ===================================
 
 def instLevaPMemo(x, end): 
     umaInstrucao = [2,x,end,-1]
@@ -85,7 +80,6 @@ def trazMemo(end):
     end = umaInstrucao[2]
     res = RAM[end]
     return res
-    
 
 
 def somar(num1, num2):
@@ -104,7 +98,7 @@ def somar(num1, num2):
     
     maquina() #maquina leva pra memoria. sem parametro pq e variavel global
 
-    umaInstrucao = instSoma(0, 1, 1) #end1 + end1 e guarda o resultado no end1
+    umaInstrucao = umaInstrucao = [0,0,1,1] #end1 + end1 e guarda o resultado no end1
     memoriaInstrucoes[0] = umaInstrucao #montou 0/0/1/1
     
     umaInstrucao = [-1] * 4 #halt
@@ -131,7 +125,7 @@ def subtrair(num1, num2):
     
     maquina()
 
-    umaInstrucao = instSubt(0, 1, 1)
+    umaInstrucao = [1,0,1,1]
     memoriaInstrucoes[0] = umaInstrucao
 
     umaInstrucao = [-1] * 4 #ghhalt
@@ -144,28 +138,34 @@ def subtrair(num1, num2):
     
     return resultado
 
-def multip(num1, num2): #??
+def multip(num1, num2): 
     umaInstrucao = [0] * 4
     
-    umaInstrucao = instLevaPMemo(num1, 0)#leva o valor n1 pra posicao 0
+    umaInstrucao = instLevaPMemo(abs(num1), 0)#leva o valor n1 pra posicao 0
     memoriaInstrucoes[0] = umaInstrucao
     
     umaInstrucao = instLevaPMemo(0, 1)#leva o valor 0 pra posicao 1
     memoriaInstrucoes[1] = umaInstrucao
     
-    for i in range(num2): #tentar criar uma funcao pra ficar aqui dentro
-        umaInstrucao = instSoma(0, 1, 1) #vai montar 0 | 0 | 1 | 1
-        #print(i)
+
+    for i in range(abs(num2)): #tentar criar uma funcao pra ficar aqui dentro
+        umaInstrucao = [0,0,1,1] #vai montar 0 | 0 | 1 | 1
+        print(i)
         # somar | end0 tem o num1 | end1 inicialmente tem 0 | é pra onde vai o resultado (substituir). nas outras vezes do for, ja vai reaproveitar    
         memoriaInstrucoes[i+2] = umaInstrucao #+2 pq ja usei 0 e 1
     
     umaInstrucao = [-1] * 4 #halt
     memoriaInstrucoes[num2+2] = umaInstrucao #multiplicador pq i vai ate ele +2
 
-    maquina() #maquina leva pra memoria. sem parametro pq e variavel global
-    
+    maquina()
+
     resultado = trazMemo(1)
     maquinaInt(umaInstrucao)
+
+    if(((num1>=0) and (num2>=0)) or ((num1<=0) and (num2<=0))):
+        return resultado
+    else:
+        resultado = '-' + str(resultado)
     
     return resultado
 
@@ -197,9 +197,9 @@ def div(num1, num2):
     ram1 = trazMemo(0) #traz o n2 do end0
     
     while(ram0<=ram1):
-        umaInstrucao = instSubt(1,0,1) #subtrai num1(end1) de num2(end0) e coloca no end 1
+        umaInstrucao = [1,1,0,1]  #subtrai num1(end1) de num2(end0) e coloca no end 1
         maquinaInt(umaInstrucao)
-        umaInstrucao = instSoma(2,3,3) #soma o end2+end3 e poe no end3 (resultado)
+        umaInstrucao = [0,2,3,3] #soma o end2+end3 e poe no end3 (resultado)
         maquinaInt(umaInstrucao)
         
         ram1 = trazMemo(1) #traz a subtraçao parcial do end1
@@ -273,7 +273,11 @@ def raizq(num1):
         elif (ram1>ram0):
             print("Não é um quadrado perfeito.")
             return "∄ em N"
+
+
         
+
+# ========================================== GEOMETRIA PLANA ======================================================
 def fat(num1):
     umaInstrucao = instLevaPMemo(1, 0)#leva o valor 1 pra posicao 0
     memoriaInstrucoes[0] = umaInstrucao
@@ -301,18 +305,195 @@ def fat(num1):
     
     return resultado
 
-def trapezArea(num1, num2, num3): 
-    ram0 = somar(num1,num2)
-    ram0 = div(ram0, num3)
-    
-    umaInstrucao = instLevaPMemo(ram0, 5)#leva o valor ram0 pra posicao 0
+def quadrArea(num1):
+    umaInstrucao = instLevaPMemo(multip(num1,num1), 7)#leva o valor da multp pra posicao 7
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    ram0 = trazMemo(7)
+
+    umaInstrucao = instLevaPMemo(ram0, 5)#leva o valor ram0 pra posicao 5
     memoriaInstrucoes[0] = umaInstrucao
     
     maquinaInt(umaInstrucao)
     
     resultado = trazMemo(5)
+
+    return resultado
+
+def retArea(num1, num2):
+    umaInstrucao = instLevaPMemo(multip(num1,num2), 12)#leva o valor da multp pra posicao 12
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    ram0 = trazMemo(12)
+
+    umaInstrucao = instLevaPMemo(ram0, 5)#leva o valor ram0 pra posicao 5
+    memoriaInstrucoes[0] = umaInstrucao
+    
+    maquinaInt(umaInstrucao)
+    
+    resultado = trazMemo(5)
+
+    return resultado
+
+
+def paralelArea(num1, num2):
+    umaInstrucao = instLevaPMemo(multip(num1,num2), 12)#leva o valor da multp pra posicao 12
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    ram0 = trazMemo(12)
+
+    umaInstrucao = instLevaPMemo(ram0, 5)#leva o valor ram0 pra posicao 5
+    memoriaInstrucoes[0] = umaInstrucao
+    
+    maquinaInt(umaInstrucao)
+    
+    resultado = trazMemo(5)
+
+    return resultado
+
+def triArea(num1, num2):
+    umaInstrucao = instLevaPMemo(multip(num1,num2), 12)#leva o valor da multp bxh pra posicao 12
+    memoriaInstrucoes[0] = umaInstrucao
+
+    umaInstrucao = instLevaPMemo(2, 13)#leva o valor 2 pra posicao 13
+    memoriaInstrucoes[1] = umaInstrucao
+
+    maquina()
+
+    ram0 = trazMemo(12) #bxh
+    ram1 = trazMemo(13) #2
+
+    ram0 = div(ram0, ram1)
+
+    umaInstrucao = instLevaPMemo(ram0, 5)#leva o valor ram0 pra posicao 5
+    memoriaInstrucoes[0] = umaInstrucao
+    
+    maquinaInt(umaInstrucao)
+    
+    resultado = trazMemo(5)
+
+    return resultado
+
+
+def losangArea(num1, num2):
+    umaInstrucao = instLevaPMemo(multip(num1,num2), 12)#leva o valor da multp bxh pra posicao 12
+    memoriaInstrucoes[0] = umaInstrucao
+
+    umaInstrucao = instLevaPMemo(2, 13)#leva o valor 2 pra posicao 13
+    memoriaInstrucoes[1] = umaInstrucao
+
+    maquina()
+
+    ram0 = trazMemo(12) #bxh
+    ram1 = trazMemo(13) #2
+
+    ram0 = div(ram0, ram1)
+
+    umaInstrucao = instLevaPMemo(ram0, 5)#leva o valor ram0 pra posicao 5
+    memoriaInstrucoes[0] = umaInstrucao
+    
+    maquinaInt(umaInstrucao)
+    
+    resultado = trazMemo(5)
+
+    return resultado
+
+def trapezArea(num1, num2, num3): 
+    umaInstrucao = instLevaPMemo(somar(num1,num2), 9)#leva o valor da soma pra posicao 9
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    ram0 = trazMemo(9)
+
+    umaInstrucao = instLevaPMemo(multip(ram0,num3), 10)#leva o valor da multp (b+b)h pra posicao 10
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    ram0 = trazMemo(10)
+
+    umaInstrucao = instLevaPMemo(div(ram0,2), 74)#leva o valor da multp (b+b)h/2 pra posicao 10
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    resultado = trazMemo(74)
+    
     return resultado
     
+
+# ========================================== GEOMETRIA ESPACIAL ======================================================
+def cubArea(num1):
+    umaInstrucao = instLevaPMemo(multip(num1,num1), 14)#leva o valor l^2 da soma pra posicao 14
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    ram0= trazMemo(14)
+
+    umaInstrucao = instLevaPMemo(multip(ram0,6), 15)#leva o valor*6 da soma pra posicao 14
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    resultado = trazMemo(15)
+
+    return resultado
+
+def cubVol(num1):
+    umaInstrucao = instLevaPMemo(pot(num1,3), 90)#leva o valor da soma pra posicao 90
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    resultado = trazMemo(90)
+    return resultado
+
+def paralVol(num1,num2,num3):
+    umaInstrucao = instLevaPMemo(multip(num1,num2), 25)#leva o valor da multp pra posicao 12
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    ram0 = trazMemo(25)
+
+    umaInstrucao = instLevaPMemo(multip(ram0,num3), 26)#leva o valor da multp pra posicao 12
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    resultado = trazMemo(26)
+    return resultado
+
+def paralArea(num1,num2,num3):
+
+    ram0 = multip(num1,num2)
+    ram1 = multip(num1,num3)
+    ram2 = multip(num2,num3)
+
+    ram0 = somar(ram0,ram1)
+    ram0 = somar(ram0,ram2)
+
+    ram0 = multip(2,ram0)
+
+    umaInstrucao = instLevaPMemo(ram0, 12)#leva o valor da ram0 pra posicao 12
+    memoriaInstrucoes[0] = umaInstrucao
+
+    maquinaInt(umaInstrucao)
+
+    resultado = trazMemo(12)
+
+    return resultado
+
+
+
 
 def menu():
     print("\t\t+-+-+-+-+-+-+-+ +-+-+-+")
@@ -338,6 +519,11 @@ def menu():
     print("11) Área de Losango")
     print("12) Área de Trapézio")
     print("13) Área de Paralelogramo\n")
+
+    print("============= Operações: Geometria Espacial ============")
+    print("14) Área de Cubo")
+    print("15) Volume de Cubo")
+    print("16) Área de Paralelepípedo")
     
     print("============= Opções da Máquina ========================")
     print("-1) Sair")
@@ -369,7 +555,7 @@ while op != -1:
         print("\n3) Multiplicação")
         num1 = int(input("Digite o multiplicando: "))
         num2 = int(input("Digite o multiplicador: "))
-        if(num1>=num2):
+        if(abs(num1)>=abs(num2)):
             print("Multiplicação de {} x {} = {}".format(num1, num2, multip(num1, num2)))
         else:
             print("Multiplicação de {} x {} = {}".format(num1, num2, multip(num2, num1)))
@@ -394,22 +580,22 @@ while op != -1:
     elif op ==8:
         print("\n8) Área de Quadrado")
         num1 = int(input("Digite a medida do lado: "))
-        print("Área de Quadrado de lado {}U.C. = {}U.A.".format(num1, multip(num1,num1)))
+        print("Área de Quadrado de lado {}U.C. = {}U.A.".format(num1, quadrArea(num1)))
     elif op ==9:
         print("\n9) Área de Retângulo")
         num1 = int(input("Digite a medida da base: "))
         num2 = int(input("Digite a medida da altura: "))
-        print("Área de Retângulo de base {}U.C. x altura {}U.C. = {}U.A.".format(num1, num2, multip(num1,num2)))
+        print("Área de Retângulo de base {}U.C. x altura {}U.C. = {}U.A.".format(num1, num2, retArea(num1,num2)))
     elif op ==10:
         print("\n10) Área de Trigângulo")
         num1 = int(input("Digite a medida da base: "))
         num2 = int(input("Digite a medida da altura: "))
-        print("Área de Triângulo de base {}U.C. x altura {}U.C. = {}U.A.".format(num1, num2, div(multip(num1,num2),2)))
+        print("Área de Triângulo de base {}U.C. x altura {}U.C. = {}U.A.".format(num1, num2, triArea(num1, num2)))
     elif op ==11:
         print("\n11) Área de Losango")
         num1 = int(input("Digite a diagonal maior: "))
         num2 = int(input("Digite a diagonal menor: "))
-        print("Área de Losango de D {}U.C. x d {}U.C. = {}U.A.".format(num1, num2, div(multip(num1,num2),2)))
+        print("Área de Losango de D {}U.C. x d {}U.C. = {}U.A.".format(num1, num2, losangArea(num1,num2)))
     elif op ==12:
         print("\n11) Área de Trapézio")
         num1 = int(input("Digite a base maior: "))
@@ -420,7 +606,28 @@ while op != -1:
         print("\n13) Área de Paralelogramo")
         num1 = int(input("Digite a medida da base: "))
         num2 = int(input("Digite a medida da altura: "))
-        print("Área de Paralelogramo de base {}U.C. x altura {}U.C. = {}U.A.".format(num1, num2, multip(num1,num2)))
+        print("Área de Paralelogramo de base {}U.C. x altura {}U.C. = {}U.A.".format(num1, num2,  paralelArea(num1, num2)))
+    elif op ==14:
+        print("\n14) Área de Cubo")
+        num1 = int(input("Digite a medida da aresta: "))
+        print("Área de cubo de aresta {}U.C = {}U.V.".format(num1, cubArea(num1)))
+    elif op ==15:
+        print("\n15) Volume de Cubo")
+        num1 = int(input("Digite a medida da aresta: "))
+        print("Volume de cubo de aresta {}U.C = {}U.V.".format(num1, cubVol(num1)))
+    elif op ==16:
+        print("\n16) Área de Paralelepípedo")
+        num1 = int(input("Digite a medida da aresta A: "))
+        num2 = int(input("Digite a medida da aresta B: "))
+        num3 = int(input("Digite a medida da aresta C: "))
+        print("Área de Paralelepípedo de arestas {} x {} x {}U.C = {}U.V.".format(num1,num2,num3, paralArea(num1,num2,num3)))
+    elif op ==17:
+        print("\n14) Volume de Paralelepípedo")
+        num1 = int(input("Digite a medida da aresta A: "))
+        num2 = int(input("Digite a medida da aresta B: "))
+        num3 = int(input("Digite a medida da aresta C: "))
+        print("Volume de Paralelepípedo de arestas {} x {} x {}U.C = {}U.V.".format(num1,num2,num3, paralVol(num1,num2,num3)))
+
     elif op ==-2:
         menu()
     else:
