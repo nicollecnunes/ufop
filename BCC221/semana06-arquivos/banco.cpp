b1f2ece853d24b0a962d4205c432309b896f7eda
Nic <nicollecanuton@gmail.com>
2021-11-09 14:32:13 -0300

    gerente: poo

--- BCC221/semana06-arquivos/banco.cpp
+++ BCC221/semana06-arquivos/banco.cpp
@@ -6,7 +6,33 @@
 
 using namespace std;
 
-void outputLine(int, string, double);
+enum requestType{ZERO_BALANCE = 1, CREDIT_BALANCE, DEBIT_BALANCE, END};
+
+int getRequest(){
+    int opc;
+    cout << "\n\nSELECIONE UMA OPCAO DE FILTRO:\n";
+    cout << "[1] ZERO BALANCE\n[2] CREDIT BALANCE\n[3] DEBIT BALANCE\n[4] END\n\n>>";
+    cin >> opc;
+    return opc;
+}
+
+void outputLine(int account, string name, double balance){
+    cout << account << " " <<  name << " " <<  balance << "\n";
+}
+
+bool shouldDisplay(int type, double balance){
+    if (type == ZERO_BALANCE && balance == 0)
+        return true;
+
+    if (type == CREDIT_BALANCE && balance < 0)
+        return true;
+
+    if (type == DEBIT_BALANCE && balance > 0)
+        return true;
+    
+    return false;
+
+}
 
 int main(){
     ofstream outClientFile("clients.dat", ios::out);
@@ -40,11 +66,46 @@ int main(){
         exit(1);
     }
 
-    cout << left << setw(10) << "Account" << setw(13) << "Name" << "Balance" << endl << fixed << showpoint;
+    int request;
+    request = getRequest();
 
-    // exibe cada registro
+    while (request != END){
+        switch(request){
+            case ZERO_BALANCE:
+                cout << "\nAcc with ZERO balance:\n";
+                break;
+            case CREDIT_BALANCE:
+                cout << "\nAcc with CREDIT balance:\n";
+                break;
+            case DEBIT_BALANCE:
+                cout << "\nAcc with DEBIT balance:\n";
+                break;
+        }
+        inClientFile >> account >> name >> balance; //leitura
+        // exibe
+        while(!inClientFile.eof()){
+            //exibe
+            if(shouldDisplay(request, balance))
+                outputLine(account, name, balance);
+            
+            // le a conta, nome e saldo
+            inClientFile >> account >> name >> balance; //leitura
+        }
+        //redefine eof para a prox entrada
+        inClientFile.clear();
+        //posiciona no comeco do arquivo
+        inClientFile.seekg(0);
+        //otem a solicitacao nova do usuario
+        request = getRequest();
+    }
+
+    cout << "end";
+
+    /*cout << left << setw(10) << "Account" << setw(13) << "Name" << "Balance" << endl << fixed << showpoint;
+
+    exibe cada registro
     while(inClientFile >> account >> name >> balance)
-        cout << account << " " <<  name << " " <<  balance << "\n";
+        outputLine(account, name, balance);*/
 
     return 0;
 }
