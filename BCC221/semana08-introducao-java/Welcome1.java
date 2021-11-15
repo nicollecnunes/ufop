61433bab95929a346be38bace7f21fdee3741204
Nic <nicollecanuton@gmail.com>
2021-11-15 13:05:31 -0300

    POO: introducao a java

---
+++ BCC221/semana08-introducao-java/Welcome1.java
@@ -0,0 +1,10 @@
+public class Welcome1
+{
+    // static indica que sera executado sem instanciar
+    public static void main(String args[])
+    {
+        System.out.println("Hello, world!");
+        System.out.print("Hello, world!\n");
+        System.out.printf("%s\n%s\n", "Hello", "world!");
+    }
+}
