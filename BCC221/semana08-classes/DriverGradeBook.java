1e8add6483917a009b2d409db841439a3c7af303
Nic <nicollecanuton@gmail.com>
2021-11-15 21:59:13 -0300

    poo: gradebook

--- BCC221/semana08-classes/DriverGradeBook.java
+++ BCC221/semana08-classes/DriverGradeBook.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
+import javax.sound.sampled.SourceDataLine;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class DriverGradeBook {
     public static void main (String args[])
     {
         GradeBook meuDiario =  new GradeBook();
-        Scanner in = new Scanner(System.in);
+        System.out.printf("O nome do  curso inicialmente e: %s\n\n", meuDiario.getNomeCurso());
 
+        Scanner in = new Scanner(System.in);
         System.out.print("Digite o nome do curso: ");
         String nome = in.nextLine();
-        
-        meuDiario.displayMessage(nome);
+
+        meuDiario.setNomeCurso(nome);
+        meuDiario.displayMessage(meuDiario.getNomeCurso());
     }
 }
--- BCC221/semana08-classes/GradeBook.java
+++ BCC221/semana08-classes/GradeBook.java
@@ -1,7 +1,18 @@
 public class GradeBook
 {
+    private String nomeCurso;
+    public void setNomeCurso(String _nomeCurso)
+    {
+        nomeCurso = _nomeCurso;
+    }
+
+    public String getNomeCurso()
+    {
+        return nomeCurso;
+    }
+
     public void displayMessage(String nomeCurso)
     {
-        System.out.printf("Bem vindo ao curso %s :)", nomeCurso);
+        System.out.printf("Bem vindo ao curso %s :)", getNomeCurso());
     }
 }
