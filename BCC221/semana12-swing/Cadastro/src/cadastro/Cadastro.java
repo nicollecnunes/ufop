58da233397d29429338c19a1b646c29f27950058
nicolle <nicollecanuton@gmail.com>
2021-12-23 14:13:59 -0300

    java swing

---
+++ BCC221/semana12-swing/Cadastro/src/cadastro/Cadastro.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cadastro;
+
+import telas.TelaPrincipal;
+
+/**
+ *
+ * @author DPCDTI
+ */
+public class Cadastro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Sistema online");
+        new TelaPrincipal().setVisible(true);
+    }
+    
+}
