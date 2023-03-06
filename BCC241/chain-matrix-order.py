50057dd5d1cf78e4566394d802db45ef3b2eeac5
nicolle <nicollecanuton@gmail.com>
2023-03-06 10:13:26 -0300

    Update chain-matrix-order.py

--- BCC241/chain-matrix-order.py
+++ BCC241/chain-matrix-order.py
@@ -18,22 +18,22 @@ d = [10,20,3,4,30]
 
 #d = [30,1,40,10,25]
 
-n = len(d)-1 
-m = [[-1 for x in range(n)] for y in range(n)]
-c = [[-1 for x in range(n)] for y in range(n)]
+numMat = len(d)-1 
+m = [[-1 for x in range(numMat)] for y in range(numMat)]
+mCusto = [[-1 for x in range(numMat)] for y in range(numMat)]
 
-for i in range(n):
+for i in range(numMat):
     m[i][i] = 0
 
-for l in range(1,n):
-    for i in range(n-l):
-        j = i + l
+for desloc in range(1,numMat):
+    for i in range(numMat-desloc): #preenche o triangulo superior
+        j = i + desloc
         m[i][j] = math.inf
-        for k in range(i,j): #k vai de i ate j-1
-            q = m[i][k] + m[k+1][j] + d[i-1] * d[k] * d[j] #q = mij
-            if (q < m[i][j]):
-                m[i][j] = q
-                c[i][j] = k #c tem o valor do k. que e a escolha, que criou o menor q
+        for k in range(i,j): #k vai de i ate j-1, testa onde encaixa parentese
+            custo = m[i][k] + m[k+1][j] + d[i-1] * d[k] * d[j] #custo = mij
+            if (custo < m[i][j]):
+                m[i][j] = custo
+                mCusto[i][j] = k #mCusto tem o valor do k. que e a escolha, que criou o menor custo
 
 
 #para criar os parenteses
@@ -43,7 +43,7 @@ def resultadoc(s, i, j):
         resultadoc(s, s[i][j] + 1, j)
         print("A" + str(i) + " * A" + str(j))
         
-def printParenthesis(i , j, n, bracket):
+def printParenthesis(i , j, numMat, bracket):
     global name
     if (i == j):
         print(name, end = "");
@@ -51,13 +51,13 @@ def printParenthesis(i , j, n, bracket):
         return;
      
     print("(", end = "");
-    printParenthesis(i, bracket[i][j], n, bracket);
-    printParenthesis(bracket[i][j] + 1, j, n, bracket);
+    printParenthesis(i, bracket[i][j], numMat, bracket);
+    printParenthesis(bracket[i][j] + 1, j, numMat, bracket);
     print(")", end = "");
 
 imprime(m)
-imprime(c)
-print("\ncusto:", m[0][n-1]);
-resultadoc(c,1, len(c)-1)
+imprime(mCusto)
+print("\ncusto:", m[0][numMat-1]);
+resultadoc(mCusto,1, len(mCusto)-1)
 name = 'A';
-printParenthesis(1, n-1, n, c)
+printParenthesis(1, numMat-1, numMat, mCusto)
